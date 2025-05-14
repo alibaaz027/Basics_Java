79e5c5424b6caa522ab8002ac15accc3605173ec
alibaaz027 <alijaved2727@gamil.com>
2025-05-15 01:46:31 +0530

    add hello murtaza in HelloWorld.java

--- java_practice/src/main/java/Basics/HelloWorld.java
+++ java_practice/src/main/java/Basics/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello javed");
+        System.out.println("hello murtaza");
     }
 }
